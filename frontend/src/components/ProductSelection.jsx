a780b3c6b36cdd04ce058f1db9459c9053eecab2
Egorick <gegor433@gmail.com>
2024-11-01 19:43:35 +0300

    Добавил возмодность добавлять в корзину продукт через его отедельную страницу
    Took 50 minutes

--- frontend/src/components/ProductSelection.jsx
+++ frontend/src/components/ProductSelection.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import SizeBlock from '../util/SizeBlock'
 
 export default function ProductSelection() {
-    const { type,id } = useParams()
+    const { type,id } = useParams();
     const [info, setInfo] = useState({
         img: '',
         brand: '',
@@ -11,13 +11,34 @@ export default function ProductSelection() {
         price: 0,
         priceWithDiscount: 0,
         color: '',
-        sizes: {}
+        discount: 0,
+        sizes: []
     })
     const [loading, setLoading] = useState(true)
+    const [sizeProduct, setTakingSize] = useState();
 
     useEffect(() => {
         console.log(info)
     }, [info])
+
+    const handleClickBasket = async () => {
+        const token = localStorage.getItem('token')
+        const productId = id
+
+        const credentials = { productId, token, sizeProduct }
+
+        try {
+            await fetch('http://localhost:8050/basket/save', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(credentials),
+            })
+        } catch (e) {
+            console.log(e)
+        }
+    }
 
     useEffect(() => {
         setLoading(true)
@@ -32,8 +53,10 @@ export default function ProductSelection() {
                     price: data.product.price || 0,
                     priceWithDiscount: data.product.priceWithDiscount || '',
                     color: data.product.color || '',
-                    sizes: data.product.listSizes || {}
+                    sizes: data.product.listSizes || [],
+                    discount: data.product.discount || 0
                 })
+                setTakingSize(info.sizes[0])
                 setLoading(false)
             })
             .catch(error => {
@@ -59,8 +82,8 @@ export default function ProductSelection() {
                 <h3 className="main-selection-product__info__brand">{info.brand}</h3>
                 <h2 className="main-selection-product__info__description">{info.description}</h2>
                 <h1 className="main-selection-product__info__price">
-                    {info.priceWithDiscount ? <p id = "discount">{info.priceWithDiscount} ₽</p> : ''}
-                    {info.priceWithDiscount ? <p id = "oldPrice">{info.price} ₽</p> : `${info.price} ₽`}
+                    {info.discount || info.discount !== 0 ? <p id = "discount">{info.priceWithDiscount} ₽</p> : ''}
+                    {info.discount || info.discount !== 0 ? <p id = "oldPrice">{info.price} ₽</p> : `${info.price} ₽`}
                 </h1>
                 <p className="main-selection-product__info__color">Цвет: <p>{info.color}</p></p>
                 <div className="main-selection-product__info__sizes">
@@ -70,12 +93,18 @@ export default function ProductSelection() {
                     <div className="main-selection-product__info__sizes__components">
                             {info.sizes.map((size) =>{
                                 return(
-                                    <SizeBlock size={size}/>
+                                    <SizeBlock
+                                        size={size}
+                                        sizeProduct = {sizeProduct}
+                                        setTakingSize = {setTakingSize}
+                                    />
                                 )
                             })}
                     </div>
                 </div>
-                <button>Добавить в корзину</button>
+                <div className="main-selection-product__info__button-window" >
+                    <button className="main-selection-product__info__button-window__button" onClick={() => handleClickBasket()}>Добавить в корзину</button>
+                </div>
             </div>
         </div>
     )
--- frontend/src/util/SizeBlock.jsx
+++ frontend/src/util/SizeBlock.jsx
@@ -1,6 +1,10 @@
-export default function SizeBlock({size}) {
+import { useEffect } from 'react'
+
+export default function SizeBlock({size,sizeProduct,setTakingSize}) {
+    const activeLink = "main-selection-product__info__sizes__components__size active";
+    const defaultLink = "main-selection-product__info__sizes__components__size";
     return(
-        <div className="main-selection-product__info__sizes__components__size">
+        <div className={size === sizeProduct ? activeLink : defaultLink} onClick={() => setTakingSize(size)}>
             <p>{size}</p>
         </div>
     )
